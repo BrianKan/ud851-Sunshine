3fc266516d90768fd857b4b026377e60b831e272
briank <brian.a.kan@gmail.com>
2017-06-27 12:12:48 -0700

    finished S03.01

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,59 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Redfish on 6/27/2017.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+    private String[] mWeatherData;
+
+    public ForecastAdapter(){
+
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder{
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view){
+            super(view);
+            mWeatherTextView=(TextView)view.findViewById(R.id.tv_weather_data);
+        }
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        String textRecieved=mWeatherData[position];
+        holder.mWeatherTextView.setText(textRecieved);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mWeatherData==null)
+            return 0;
+        else
+            return mWeatherData.length;
+    }
+
+    public void setWeatherData(String[] weatherData){
+        mWeatherData=weatherData;
+        notifyDataSetChanged();
+    }
+}
